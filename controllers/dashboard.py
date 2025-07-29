f6e2e7013505fcf06d99b36a18365f9f3b4bf87f
stnmy <sakibulhaquetanmoy@gmail.com>
2025-07-29 14:38:47 +0600

    hello

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -3,6 +3,7 @@ from py4web import action, request, abort, redirect, URL
 from yatl.helpers import A
 from ..common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 
+#helloooooooooooooooooooooooooooo
 
 @action("dashboard/index")
 @action.uses("dashboard/index.html",session,flash,db)

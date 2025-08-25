7555214f914b1a71d080dbe4b5a427969780a1a6
Tamim <tamim.hossen@transcombd.com>
2025-08-25 12:52:24 +0600

    aaaaa

--- common_fn.py
+++ common_fn.py
@@ -1,2 +1,2 @@
-IMAGE_UPLOAD_API = "http://127.0.0.1:8001/upload_file_expense"
-IMAGE_DOWNLOAD_API = "http://127.0.0.1:8001/download_file"
+IMAGE_UPLOAD_API = "https://filestore.transcombd.com/upload_file_expense"
+IMAGE_DOWNLOAD_API = "https://filestore.transcombd.com/uploadimage"
